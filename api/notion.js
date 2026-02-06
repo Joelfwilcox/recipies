539d29b72d9985beb54393d3e9ce0e4a44cfd7b4
Joelfwilcox <joelfwilcox@gmail.com>
2026-02-06 20:18:02 +0000

    Fix: Change notion.js export to CommonJS for Vercel

--- api/notion.js
+++ api/notion.js
@@ -25,7 +25,7 @@ async function fetchWithRetry(url, options, retries = MAX_RETRIES) {
   }
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
