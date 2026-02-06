45e3eb57a24640fa5d62ad678e1790eadb7ffe9c
Joelfwilcox <joelfwilcox@gmail.com>
2026-02-06 20:09:43 +0000

    Fix: Change ES module export to CommonJS for Vercel

--- api/categorize.js
+++ api/categorize.js
@@ -2,7 +2,7 @@ const MAX_RETRIES = 3;
 const RETRY_DELAY = 2000;
 
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
